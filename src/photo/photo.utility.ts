32bbbdb817e39b1a57231c48ac13ea52175828ec
정훈박 <fireking5997@gmail.com>
2021-07-16 20:09:46 +0900

    master 원상복구

--- src/photo/photo.utility.ts
+++ src/photo/photo.utility.ts
@@ -3,7 +3,7 @@ export const getConnectOrCreate = (caption) => {
 
   if (caption && caption.includes("#")) {
     //중간에 | 는 뺀다.  |는 그룹 안에서만 사용할 것.
-    const tags = caption.match(/#[ㄱ-ㅎㅏ-ㅣ가-힣\w]+/gi);
+    const tags = caption.match(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/gi);
     console.log(tags);
     for (let item of tags) {
       connectOrCreate.push({
