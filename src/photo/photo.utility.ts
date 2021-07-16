f9bd4831ce797d8304fbd23cd8e5b31baaf4eb2c
정훈박 <fireking5997@gmail.com>
2021-07-16 20:12:13 +0900

    [photo.utility.ts]| 는 group 안에서만 사용

--- src/photo/photo.utility.ts
+++ src/photo/photo.utility.ts
@@ -2,7 +2,8 @@ export const getConnectOrCreate = (caption) => {
   const connectOrCreate = [];
 
   if (caption && caption.includes("#")) {
-    const tags = caption.match(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/gi);
+    //| 는 그룹 안에서만 사용!
+    const tags = caption.match(/#[ㄱ-ㅎㅏ-ㅣ가-힣\w]+/gi);
     console.log(tags);
     for (let item of tags) {
       connectOrCreate.push({
